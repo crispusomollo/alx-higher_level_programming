226cd301a6a9c2788e02218ef678fc1617167593
crispusomollo <crispusomollo@gmail.com>
2023-08-29 07:47:05 -0700

    Write a class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Private instance attribute size"""
     def __init__(self, size=0):
-        if size is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise TypeError("size must be >= 0")
