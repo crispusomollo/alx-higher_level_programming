411581e518e4fcf59bc6a368b80b9e265a4f6b73
Crispus Makokha Omollo <126664434+crispusomollo@users.noreply.github.com>
2023-09-17 12:52:10 +0300

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-for i in range(122, 96, -2):
-    rs = f"{chr(i):s}{chr(i + ord('A') - ord('a') - 1):s}"
-    print("{:s}".format(rs), end="")
--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def fizzbuzz():
-    for i in range(1, 101):
-        k = i % 5 == 0
-        m = i % 3 == 0
-        j = k and m
-        rs = f"FizzBuzz" if j else f"Buzz" if k \
-            else f"Fizz" if m else f"{i:d}"
-        print(rs, end=" ")
