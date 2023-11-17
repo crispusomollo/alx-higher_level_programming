c9d5f8fa69577af7d1bfdebdafea3f9c1c66ce97
crispusomollo <crispusomollo@gmail.com>
2023-11-17 06:44:48 -0800

    Write a script that lists all states with a name starting with N (upper N)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""List all the states"""
-
+"""return states starting with 'N'"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    #connect to the database
-    conn=MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-            user=argv[1], passwd=argv[2], db=argv[3])
-cur = conn.cursor()
-cur.execute("SELECT * FROM states where REGEXP_LIKE(name,'^n');")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-conn.close()
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         charset="utf8",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL; filter names starting with 'N'
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    for row in cursor.fetchall():
+        if row[1][0] == 'N':
+            print(row)
+    cursor.close()
+    db.close()
