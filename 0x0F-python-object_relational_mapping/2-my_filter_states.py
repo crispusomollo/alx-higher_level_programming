7eb99007da47c9b44647f6dadf62589f6aed8e24
crispusomollo <crispusomollo@gmail.com>
2023-11-17 07:12:51 -0800

    Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""script that takes in an argument and displays all values 
-in the states table of hbtn_0e_0_usa
-where name matches the argument.
 """
-
+return matching states
+parameters given to script: username, password, database, state to match
+"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, 
-            user=argv[1],passwd=argv[2],db=argv[3])
-cursor=conn.cursor()
-sql_cmd = """SELECT *
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL; match arg given
+    cursor = db.cursor()
+    sql_cmd = """SELECT *
                  FROM states
                  WHERE name LIKE '{:s}' ORDER BY id ASC""".format(argv[4])
-cursor.execute(sql_cmd)
-for row in cursor.fetchall():
-    if row[1] == argv[4]:
-        print(row)
-cursor.close()
-db.close()
+    cursor.execute(sql_cmd)
+    for row in cursor.fetchall():
+        if row[1] == argv[4]:
+            print(row)
+    cursor.close()
+    db.close()
