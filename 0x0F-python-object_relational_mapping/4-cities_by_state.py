9507f028f7048c645f1be023036164b1307439bd
crispusomollo <crispusomollo@gmail.com>
2023-11-18 08:58:18 +0000

    Write a script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Write a script that lists all cities
+from the database hbtn_0e_4_usa
+"""
+
+
+import sys
+import MySQLdb
+
+def dbConnect():
+    link=MySQLdb.connect(
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3],
+            host="localhost",
+            port=3306,
+            charset="utf8"
+            )
+    exe=link.cursor()
+    exe.execute("SELECT cities.id, cities.name, states.name from cities JOIN states WHERE states.id = cities.state_id;")
+    link.commit()
+    result=exe.fetchall()
+    for res in result:
+        print(res)
+    exe.close()
+    link.close()
+
+if __name__ == "__main__":
+    dbConnect()
