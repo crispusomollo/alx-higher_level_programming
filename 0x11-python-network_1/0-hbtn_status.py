067db276d9fd3bf1f2240ed5c8d7d32340d25ac8
crispusomollo <crispusomollo@gmail.com>
2023-12-01 04:13:32 -0800

    Write a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,9 @@
 """Fetches https://alx-intranet.hbtn.io/status"""
 
 
-if __name__ == "__main__":
-    import urllib.request
+import urllib.request
 
+if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
